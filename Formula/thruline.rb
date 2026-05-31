26a8406a65c77f605a6f245f022e9f7437fc2078
iMi <pufferhaus@fugu.estate>
2026-05-31 06:16:14 +0000

    thruline: update to v0.2.0

--- Formula/thruline.rb
+++ Formula/thruline.rb
@@ -1,8 +1,8 @@
 class Thruline < Formula
   desc "Minimal DSL and runtime for deterministic multiagent pipelines"
   homepage "https://thruline.work"
-  url "https://github.com/pufferhaus/thruline/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "cb6831236ca5efd70b986b119afebbf327e05b14cfb1b54f84ab8f6ccb978717"
+  url "https://github.com/pufferhaus/thruline/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "15efca8a60dce22fa0bd4e85e831736bf6b817fd46cdfb9b935c3532f99045ae"
   license "MIT"
   head "https://github.com/pufferhaus/thruline.git", branch: "main"
 
